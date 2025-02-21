22e888b15ad662218cd5aab3c07027e82cbfb8c1
Mikael Andersson <mikael.andersson3@ingka.ikea.com>
2025-02-21 07:31:44 +0100

    feat: install uipathcli on runner
    Download and install the uipathcli tool to GitHub Actions runner
    to enable use in further steps of a job

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import * as tc from '@actions/tool-cache'
+import { getDownloadUrl } from './uipathcli'
+import os from 'os'
 
 /**
  * The main function for the action.
@@ -8,18 +10,30 @@ import { wait } from './wait.js'
  */
 export async function run() {
   try {
-    const ms = core.getInput('milliseconds')
+    const uipathUri = core.getInput('uipath-uri')
+    const tenant = core.getInput('tenant')
+    const clientId = core.getInput('client-id')
+    const clientSecret = core.getInput('client-secret')
+    const identityUrl = core.getInput('identity-url')
+    const organization = core.getInput('organization')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const downloadUrl = await getDownloadUrl()
+    core.info(`Downloading uipathcli from ${downloadUrl}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const cliPath = await tc.downloadTool(downloadUrl)
+    var downloadPath = ''
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    if (os.type() != 'windows') {
+      core.info('Extracting tar.gz')
+      downloadPath = await tc.extractTar(cliPath)
+      core.addPath(downloadPath)
+    } else {
+      core.info('Extracting zip')
+      downloadPath = await tc.extractZip(cliPath)
+      core.addPath(downloadPath)
+    }
+    core.info(`uipathcli extracted to ${downloadPath} and added to PATH`)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)

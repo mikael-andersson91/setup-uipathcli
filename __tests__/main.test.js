e2816c3ca2e452c930c901f6ded279cf73649e81
Mikael Andersson <mikael.andersson3@ingka.ikea.com>
2025-02-23 08:27:24 +0100

    test: remove verification of wait
    Remove template actions test functions. To be replaced with actual tests

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -5,7 +5,7 @@
  * functions and objects. For example, the core module is mocked in this test,
  * so that the actual '@actions/core' module is not imported.
  */
-import { jest } from '@jest/globals'
+import { expect, jest } from '@jest/globals'
 import * as core from '../__fixtures__/core.js'
 
 // Mocks should be declared before the module being tested is imported.
@@ -18,43 +18,14 @@ const { run } = await import('../src/main.js')
 describe('main.js', () => {
   beforeEach(() => {
     // Set the action's inputs as return values from core.getInput().
-    core.getInput.mockImplementation(() => '500')
-
-    // Mock the wait function so that it does not actually wait.
-    wait.mockImplementation(() => Promise.resolve('done!'))
+    core.getInput.mockImplementation(() => 'https://cloud.uipath.com')
   })
 
   afterEach(() => {
     jest.resetAllMocks()
   })
 
-  it('Sets the time output', async () => {
+  it('Run main', async () => {
     await run()
-
-    // Verify the time output was set.
-    expect(core.setOutput).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      // Simple regex to match a time string in the format HH:MM:SS.
-      expect.stringMatching(/^\d{2}:\d{2}:\d{2}/)
-    )
-  })
-
-  it('Sets a failed status', async () => {
-    // Clear the getInput mock and return an invalid value.
-    core.getInput.mockClear().mockReturnValueOnce('this is not a number')
-
-    // Clear the wait mock and return a rejected promise.
-    wait
-      .mockClear()
-      .mockRejectedValueOnce(new Error('milliseconds is not a number'))
-
-    await run()
-
-    // Verify that the action was marked as failed.
-    expect(core.setFailed).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds is not a number'
-    )
   })
 })
